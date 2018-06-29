9ee17c6401cf303e1fb988c039b283ed7bbd86af
Patrick Silva <patrickeeep12@gmail.com>
2018-06-29 18:43:14 -0300

    Adicionando jpa em categoria

--- cursomc/src/main/java/com/tricksil/cursomc/domain/Categoria.java
+++ cursomc/src/main/java/com/tricksil/cursomc/domain/Categoria.java
@@ -5,15 +5,24 @@ import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.io.Serializable;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @EqualsAndHashCode(of = "id")
+@Entity
 public class Categoria implements Serializable {
+    private static final long serialVersionUID = 1L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+
     private String nome;
 
 }
